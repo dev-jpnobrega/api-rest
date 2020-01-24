5ff21a79d29ad52620b33f69aa3df9bf87aafb0b
João Paulo Nobrega <joao.paulo.nobrega@avanade.com>
2020-01-24 15:52:00 -0300

    add fc

--- src/infrastructure/routers/routers.go
+++ src/infrastructure/routers/routers.go
@@ -6,7 +6,6 @@ import (
 	interfaces "github.com/dev-jpnobrega/api-rest/src/domain/contract/interface"
 	"github.com/dev-jpnobrega/api-rest/src/infrastructure/factory"
 	handler "github.com/dev-jpnobrega/api-rest/src/infrastructure/http"
-	"github.com/gorilla/mux"
 )
 
 func adapter(command interfaces.ICommand, h handler.IHandler) func(w http.ResponseWriter, r *http.Request) {
@@ -16,16 +15,16 @@ func adapter(command interfaces.ICommand, h handler.IHandler) func(w http.Respon
 }
 
 // BuildRouters create router APP
-func BuildRouters() *mux.Router {
-	router := mux.NewRouter().StrictSlash(true)
+func BuildRouters(server http.ServeMux) http.ServeMux {
+	// router := mux.NewRouter().StrictSlash(true)
 
-	router.HandleFunc(
+	server.HandleFunc(
 		"/v1/people",
 		adapter(
 			factory.GetPeopleFactory(),
 			handler.NewHandler(),
 		),
-	).Methods("GET")
+	)
 
-	return router
+	return server
 }
--- src/main.go
+++ src/main.go
@@ -1,24 +1,22 @@
 package main
 
 import (
+	"context"
 	"github.com/dev-jpnobrega/api-rest/src/infrastructure/routers"
 	"log"
 	"net/http"
 )
 
-type server struct{}
-
-func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(`{"message": "hello world"}`))
+func AddContext(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Method, "-", r.RequestURI)
+		ctx := context.WithValue(r.Context(), "Username", "")
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
 
 func main() {
-	s := &server{}
+	server := http.NewServeMux()
 
-	http.Handle("/", s)
-
-	log.Fatal(http.ListenAndServe(":8080", routers.BuildRouters()))
+	log.Fatal(http.ListenAndServe(":8080", routers.BuildRouters(server)))
 }
-
